02226c795a9ed7a9e2d6e45363526dc2265b5b85
liviabritomotta <livia.motta@sptech.school>
2026-08-31 11:07:41 -0300

    feat:adicionando funções de chamarHPC e de chamarCluster

--- src/models/nodesModel.js
+++ src/models/nodesModel.js
@@ -1,5 +1,23 @@
 var database = require("../database/config");
 
+function chamarHPC(nome,empresa_fk){
+     var instrucaoSql = `SELECT ambiente_hpc.nome,
+        ambiente_hpc.empresa_fk 
+        from ambiente_hpc 
+        join empresa on 
+        empresa.id = ambiente_hpc.empresa_fk `;
+         return database.executar(instrucaoSql);
+}
+
+function chamarCluster(nome,ambiente_hpc_fk){
+     var instrucaoSql = `SELECT cluster.nome,
+        cluster.ambiente_hpc_fk 
+        from cluster join ambiente_hpc on 
+        ambiente_hpc.id_ambiente_hpc = cluster.ambiente_hpc_fk`;
+         return database.executar(instrucaoSql);
+}
+
+
 function cadastrar(hostname, ip, status, sistemaOperacional, clusterFk){
      var instrucaoSql = `
         INSERT INTO node
@@ -12,5 +30,6 @@ function cadastrar(hostname, ip, status, sistemaOperacional, clusterFk){
 }
 
 module.exports = {
-    cadastrar
+    cadastrar,
+    chamarHPC
 };
